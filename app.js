b725c3d6f5a82f2918de26aa4228d90d05354fd1
clickjaw <clickjaw@outlook.com>
2022-06-30 22:56:24 -0500

    fresh

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
 'use strict';
-// Create a "close" button and append it to each list item
-let myNodelist = document.getElementsByTagName("li");
-for (let i = 0; i < myNodelist.length; i++) {
-  let span = document.createElement("span");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-let close = document.getElementsByClassName("close");
-for (let i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    let div = this.parentElement;
-    div.style.display = "none";
-  }
-}
 
 // Create a new list item
 function newElement() {
@@ -28,16 +10,5 @@ function newElement() {
 
   document.getElementById("toDoItem").value = "";
 
-  let span = document.createElement("span");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      let div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+ 
 }
