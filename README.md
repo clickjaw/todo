319d20b16df3ac92197cc2544a1f3b12f5e9b1ab
clickjaw <clickjaw@outlook.com>
2022-06-30 22:26:57 -0500

    phone check

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # todo
+
+a simple to do list that when entered logs the entry to the webpage
+
+functionality: TBD;
---
+++ app.js
@@ -0,0 +1,44 @@
+'use strict';
+// Create a "close" button and append it to each list item
+let myNodelist = document.getElementsByTagName("li
+");
+for (let i = 0; i < myNodelist.length; i++) {
+  let span = document.createElement("span");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Create a new list item
+function newElement() {
+  let li = document.createElement("li");
+  let toDoItem = document.getElementById("toDoItem").value;
+  let t = document.createTextNode(toDoItem);
+  li.appendChild(t);
+  document.getElementById('myUL').appendChild(li);
+
+  document.getElementById("toDoItem").value = "";
+
+  let span = document.createElement("span");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
